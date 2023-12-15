866b60ae95287db2bbf9a239d977c1060ca42a15
Pompon <103881805+p0mpon@users.noreply.github.com>
2023-12-15 21:27:18 +0100

    stodoła i produkt testy

--- Projekt_Farma/Main.java
+++ Projekt_Farma/Main.java
@@ -1,4 +1,6 @@
 import Zboża.Zboża;
+import Stodola.Stodola;
+import Produkt.Produkt;
 
 public class Main {
     public static void main(String[] args)
@@ -22,6 +24,20 @@ public class Main {
 //        frame.setVisible(true);
         Zboża zboże1 = new Zboża();
         zboże1.getStan();
+
+        //tworzenie stodoly i dodawanie do niej produktow
+        Stodola stodola = new Stodola();
+        stodola.wyswietlProdukty();
+
+        Produkt produkt1 = new Produkt("Pszenica",false,20,30,"adres");
+        produkt1.zbierz();
+        produkt1.setGotoweDoZebrania(true);
+        produkt1.zbierz();
+
+        Produkt produkt2 = new Produkt("Jablko",true,10,15,"adres");
+        produkt2.zbierz();
+
+        stodola.wyswietlProdukty();
     }
 }
 
