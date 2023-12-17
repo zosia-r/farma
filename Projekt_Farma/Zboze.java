c6de28c2f24aa89a52b886d910d9fdf0fc4b04aa
zosia-r <148854197+zosia-r@users.noreply.github.com>
2023-12-17 21:31:42 +0100

    Add files via upload

---
+++ Projekt_Farma/Zboze.java
@@ -0,0 +1,25 @@
+package Pole;
+
+public class Zboze extends Uprawa
+{
+    private boolean czyPasza;
+
+    public Zboze(String nazwa, boolean gotoweDoZebrania, int cena, int czasProdukcji, String adresIkonki,boolean czyPasza)
+    {
+        super(nazwa, gotoweDoZebrania, cena, czasProdukcji, adresIkonki);
+        this.czyPasza = czyPasza;
+    }
+
+    public boolean getCzyPasza() {
+        return czyPasza;
+    }
+
+    public void setCzyPasza(boolean czyPasza) {
+        this.czyPasza = czyPasza;
+    }
+
+    public void wytworzPasze()
+    {
+        czyPasza = true;
+    }
+}
