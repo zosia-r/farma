889151574fe2b6e31433a38a3f06565e8c947f3c
zosia-r <148854197+zosia-r@users.noreply.github.com>
2023-12-30 21:32:05 +0100

    Update Main.java

--- Projekt_Farma/Main.java
+++ Projekt_Farma/Main.java
@@ -12,18 +12,18 @@ public class Main {
         Stodola.getInstance();
         Stodola.getInstance().wyswietlProdukty();
 
-        Produkt produkt1 = new Produkt("pszenica",false,20,30,"adres");
+        Produkt produkt1 = new Produkt("pszenica",false,20,5, 30,"adres");
         produkt1.zbierz();
         produkt1.setGotoweDoZebrania(true);
         produkt1.zbierz();
 
-        Produkt produkt2 = new Produkt("jabłka",true,10,15,"adres");
+        Produkt produkt2 = new Produkt("jabłka",true,10,2, 15,"adres");
         produkt2.zbierz();
 
         Stodola.getInstance().wyswietlProdukty();
 
         Pole_Uprawne p = new Pole_Uprawne(1, 2);
-        Owoc o = new Owoc("jabłko", true, 2, 1, "a", true, true);
+        Owoc o = new Owoc("jabłko", true, 2, 1,1, "a", true, true);
         p.zasadz(o);
         p.zasadz(o);
 
