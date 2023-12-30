388b0a4d66cfb0ab99c36239ee6af2c5199a641c
zosia-r <148854197+zosia-r@users.noreply.github.com>
2023-12-30 21:16:05 +0100

    singleton + dwie ceny

--- Projekt_Farma/Stodola/Stodola.java
+++ Projekt_Farma/Stodola/Stodola.java
@@ -9,34 +9,47 @@ import java.util.Iterator;
 
 public class Stodola {
 
+    private static Stodola instance;
     static private Map<Produkt, Integer> produkty = new HashMap<>();
 
-    public Stodola()
+    private Stodola()
     {
         //rośliny paszowe
-        produkty.put(new Produkt("pszenica",true,20,10,"ad"),0);
-        produkty.put(new Produkt("żyto", true, 22, 10, "ad3"), 0);
+        produkty.put(new Produkt("pszenica",true,20,5,10,"ad"),0);
+        produkty.put(new Produkt("żyto", true, 22, 5,10, "ad3"), 0);
         //owoce
-        produkty.put(new Produkt("jabłka",true,10,15,"ad"),0);
-        produkty.put(new Produkt ("gruszki", true, 12, 15, "ad"), 0);
-        produkty.put(new Produkt("winogrona", true, 15, 15, "ad"), 0);
+        produkty.put(new Produkt("jabłka",true,10,2, 15,"ad"),0);
+        produkty.put(new Produkt ("gruszki", true, 12,2,  15, "ad"), 0);
+        produkty.put(new Produkt("winogrona", true, 15,3,  15, "ad"), 0);
         //odzwierzęce
-        produkty.put(new Produkt("mleko", true, 15, 10, "ad"), 0);
-        produkty.put(new Produkt("jajka", true, 16, 10, "ad"), 0);
-        produkty.put(new Produkt("mięso", true, 25, 10,"ad"), 0);
+        produkty.put(new Produkt("mleko", true, 15, 3, 10, "ad"), 0);
+        produkty.put(new Produkt("jajka", true, 16, 4, 10, "ad"), 0);
+        produkty.put(new Produkt("mięso", true, 25, 6, 10,"ad"), 0);
         //przetwory
-        produkty.put(new Produkt("chleb pszenny", true, 44, 10, "ad"), 0);
-        produkty.put(new Produkt("chleb żytni", true, 45, 10, "ad"), 0);
-        produkty.put(new Produkt("dżem jabłkowy", true, 37, 8, "ad"), 0);
-        produkty.put(new Produkt("dżem gruszkowy", true, 38, 8, "ad"), 0);
-        produkty.put(new Produkt("sok jabłkowy", true, 35, 9, "ad"), 0);
-        produkty.put(new Produkt("sok gruszkowy", true, 36, 9, "ad"), 0);
-        produkty.put(new Produkt("masło", true, 40, 10, "ad"), 0);
-        produkty.put(new Produkt("szynka", true, 55, 12, "ad"), 0);
-        produkty.put(new Produkt("kiełbasa", true, 56, 12, "ad"), 0);
+        produkty.put(new Produkt("chleb pszenny", true, 44, 11, 10, "ad"), 0);
+        produkty.put(new Produkt("chleb żytni", true, 45, 11, 10, "ad"), 0);
+        produkty.put(new Produkt("dżem jabłkowy", true, 37, 9, 8, "ad"), 0);
+        produkty.put(new Produkt("dżem gruszkowy", true, 38, 9, 8, "ad"), 0);
+        produkty.put(new Produkt("sok jabłkowy", true, 35, 8, 9, "ad"), 0);
+        produkty.put(new Produkt("sok gruszkowy", true, 36, 9, 9, "ad"), 0);
+        produkty.put(new Produkt("masło", true, 40, 10, 10, "ad"), 0);
+        produkty.put(new Produkt("szynka", true, 55, 13, 12, "ad"), 0);
+        produkty.put(new Produkt("kiełbasa", true, 56, 14, 12, "ad"), 0);
+
     }
 
-    public static void dodajProdukt(Produkt p)
+    public static Stodola getInstance()
+    {
+        if (instance == null)
+            instance = new Stodola();
+        return instance;
+    }
+
+    public Map<Produkt, Integer> getProdukty()
+    {
+        return produkty;
+    }
+    public void dodajProdukt(Produkt p)
     {
         Set<Map.Entry<Produkt,Integer>> entries = produkty.entrySet();
         Iterator<Map.Entry<Produkt,Integer>> iteratorProduktow = entries.iterator();
@@ -64,7 +77,6 @@ public class Stodola {
             }
         }
     }
-
     public void wyswietlProdukty()
     {
         Set<Map.Entry<Produkt,Integer>> entries = produkty.entrySet();
@@ -85,6 +97,16 @@ public class Stodola {
             Map.Entry<Produkt, Integer> entry = iteratorProduktow.next();
             if (nazwa.equals(entry.getKey().getNazwa()))
                 return entry.getValue();
+        }
+        return 0;
+    }
+
+    public int getCenaProduktu(String nazwa)
+    {
+        for (Produkt produkt : produkty.keySet())
+        {
+            if (produkt.getNazwa().equals(nazwa))
+                return produkt.getCena();
         }
         return 0;
     }
