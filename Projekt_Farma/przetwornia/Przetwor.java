6663df3d4101c0c5ee892b784ee2ddb96840aa1e
rozamazurek <rozamaz2004@wp.pl>
2023-12-30 11:15:23 +0100

    dodanie klasy Przetwor

---
+++ Projekt_Farma/przetwornia/Przetwor.java
@@ -0,0 +1,62 @@
+package przetwornia;
+import Produkt.Produkt;
+import java.util.Timer;
+import java.util.TimerTask;
+import Stodola.Stodola;
+
+public class Przetwor extends Produkt {
+    private Produkt ProduktPotrzebnyDoWyprodukowania;
+    private int iloscPotrzebnegoProduktu;
+    private Timer odliczanieTimer;
+    private Timer wzrostTimer;
+
+    public Przetwor(String nazwa, boolean gotoweDoZebrania, int cena, int czasProdukcji, String adresIkonki, Produkt ProduktPotrzebnyDoWyprodukowania, int iloscPotrzebnegoProduktu) {
+        super(nazwa, gotoweDoZebrania, cena, czasProdukcji, adresIkonki);
+        int l = Stodola.sprawdzDostepnosc(ProduktPotrzebnyDoWyprodukowania.getNazwa());
+        if (l >= iloscPotrzebnegoProduktu) {
+            uruchomLicznikCzasu();
+            uruchomLicznikOdliczania();
+            for (int i = 0; i <= iloscPotrzebnegoProduktu; i++) {
+                Stodola.usunProdukt(ProduktPotrzebnyDoWyprodukowania);
+                }
+            }
+        }
+    public void przerwijProdukcjePrzet() {
+        setGotoweDoZebrania(false);
+        wzrostTimer.cancel();
+        odliczanieTimer.cancel();
+        for (int i = 0; i <= iloscPotrzebnegoProduktu; i++) {
+            Stodola.dodajProdukt(ProduktPotrzebnyDoWyprodukowania);
+        }
+
+    }
+
+    private void uruchomLicznikCzasu() {
+        wzrostTimer = new Timer();
+        wzrostTimer.schedule(new TimerTask() {
+            public void run() {
+                setGotoweDoZebrania(true);
+                System.out.println("Przetwor jest gotowy do zebrania");
+                wzrostTimer.cancel();
+            }
+        }, getCzasProdukcji() * 1000);
+    }
+
+    private void uruchomLicznikOdliczania() {
+        odliczanieTimer = new Timer();
+        odliczanieTimer.scheduleAtFixedRate(new TimerTask() {
+            int czasDoZebrania = getCzasProdukcji();
+
+            public void run() {
+                czasDoZebrania--;
+                System.out.println("Pozostały czas do końca produkcji: " + czasDoZebrania + " sekund");
+                if (czasDoZebrania <= 0) {
+                    odliczanieTimer.cancel();
+                }
+            }
+        }, 1000, 1000);
+    }
+}
+
+
+
