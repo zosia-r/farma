cb2b6e9b22559852108ffd0bf857e03f749d13b4
zosia-r <148854197+zosia-r@users.noreply.github.com>
2024-01-01 23:37:23 +0100

    Update Farma.java

--- Projekt_Farma/Farma/Farma.java
+++ Projekt_Farma/Farma/Farma.java
@@ -1,25 +1,23 @@
 package Farma;
 
 import Kawalek_Ziemi.Kawalek_Ziemi;
+import Stodola.Stodola;
 
 public class Farma
 {
-    private static Farma instance;
     private String nazwaFarmy;  // wyświetlana nazwa farmy gracza
     private int wymiar;   // wymiar farmy, długość boku
     private Kawalek_Ziemi[][] kawalki_ziemi; // tablica dwuwymiarowa o bokach długości wymiar x wymiar
-    private Farma()
+    private Stodola stodola;
+    public Farma()
     {
         nazwaFarmy="FARMA";
         wymiar = 5;
         kawalki_ziemi = new Kawalek_Ziemi[wymiar][wymiar];
+        stodola = new Stodola();
     }
-    public static Farma getInstance()
-    {
-        if (instance == null)
-            instance = new Farma();
-        return instance;
-    }
+
+    public Stodola getStodola() { return stodola; }
     public String getNazwaFarmy()
     {
         return nazwaFarmy;
@@ -32,6 +30,7 @@ public class Farma
     {
         return kawalki_ziemi;
     }
+    public void setStodola(Stodola stodola) { this.stodola = stodola; }
     public void setNazwaFarmy(String nazwaFarmy)
     {
         this.nazwaFarmy=nazwaFarmy;
