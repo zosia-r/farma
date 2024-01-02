e3e1f6e96947ee925e7405b236850c0e877f9fdc
rozamazurek <rozamaz2004@wp.pl>
2024-01-02 15:24:21 +0100

    Timery do produktów

--- Projekt_Farma/Pole/Uprawa.java
+++ Projekt_Farma/Pole/Uprawa.java
@@ -5,53 +5,21 @@ import Produkt.Produkt;
 
 public abstract class Uprawa extends Produkt
 {
-    private Timer odliczanieTimer;
-    private Timer wzrostTimer;
 
     public Uprawa(String nazwa, boolean gotoweDoZebrania, int cena, int cenaWytworzenia, int czasProdukcji, String adresIkonki)
     {
         super(nazwa, gotoweDoZebrania, cena, cenaWytworzenia, czasProdukcji, adresIkonki);
-        uruchomLicznikCzasu();
-        uruchomLicznikOdliczania();
+        uruchomLiczniki();
 
     }
 
     public void przerwijWzrost()
     {
         setGotoweDoZebrania(false);
-        wzrostTimer.cancel();
-        odliczanieTimer.cancel();
+        getWzrostTimer().cancel();
+        getOdliczanieTimer().cancel();
     }
 
-    private void uruchomLicznikCzasu()
-    {
-        wzrostTimer=new Timer();
-        wzrostTimer.schedule(new TimerTask() {
-            public void run() {
-                setGotoweDoZebrania(true);
-                System.out.println("Uprawa jest gotowa do zebrania");
-                wzrostTimer.cancel();
-            }
-        },getCzasProdukcji()*1000);
-    }
-
-    private void uruchomLicznikOdliczania()
-    {
-        odliczanieTimer = new Timer();
-        odliczanieTimer.scheduleAtFixedRate(new TimerTask()
-        {
-            int czasDoZebrania=getCzasProdukcji();
-            public void run(){
-                czasDoZebrania--;
-                System.out.println("Pozostały czas do zebrania: "+czasDoZebrania+" sekund");
-                if(czasDoZebrania<=0)
-                {
-                    odliczanieTimer.cancel();
-                }
-            }
-        },1000,1000);
-
-
-    }
 }
+
 
