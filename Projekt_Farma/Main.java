ad582c0d71458f78d90a8e21bbd1060b7710c07a
Pompon <103881805+p0mpon@users.noreply.github.com>
2024-01-02 22:24:51 +0100

    zakomentowanie kodu tworzenia instancji gry

--- Projekt_Farma/Main.java
+++ Projekt_Farma/Main.java
@@ -8,23 +8,17 @@ import GUI.*;
 public class Main {
     public static void main(String[] args)
     {
-        Gra.getInstance();
-
-        Produkt produkt1 = new Produkt("pszenica",false,20,5, 30,"adres");
-        produkt1.zbierz();
-        produkt1.setGotoweDoZebrania(true);
-        produkt1.zbierz();
-
-        Produkt produkt2 = new Produkt("jabłka",true,10,2, 15,"adres");
-        produkt2.zbierz();
+        // zeby timer dzialal poprawnie (po wcisnieciu START) nie moze byc wywolywane Gra.getInstance() w mainie,
+        // wiec finalnie bedzie tu tylko Start start = new Start();
+        
+        /* Gra.getInstance();
 
         Gra.getInstance().getFarmaGracza().getStodola().wyswietlProdukty();
 
         Pole_Uprawne p = new Pole_Uprawne(1, 2);
         Owoc o = new Owoc("jabłko", true, 2, 1,1, "a", true, true);
         p.zasadz(o);
-        p.zasadz(o);
-
+        p.zasadz(o); */
 
         ////
         Start start = new Start();
