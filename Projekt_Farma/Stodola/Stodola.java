11c7d1a610477da246ddc3571929782bf763081c
Sunariz <113854410+Sunariz@users.noreply.github.com>
2024-01-05 17:19:29 +0100

    Zmiany aby sadzenie i zbieranie działało

--- Projekt_Farma/Stodola/Stodola.java
+++ Projekt_Farma/Stodola/Stodola.java
@@ -14,26 +14,26 @@ public class Stodola {
     public Stodola()
     {
         //rośliny paszowe
-        produkty.put(new Produkt("pszenica",true,20,5,10,"ad"),0);
-        produkty.put(new Produkt("żyto", true, 22, 5,10, "ad3"), 0);
+        produkty.put(new Produkt("pszenica",false,20,5,10,"ad"),0);
+        produkty.put(new Produkt("żyto", false, 22, 5,10, "ad3"), 0);
         //owoce
-        produkty.put(new Produkt("jabłka",true,10,2, 15,"ad"),0);
-        produkty.put(new Produkt ("gruszki", true, 12,2,  15, "ad"), 0);
-        produkty.put(new Produkt("winogrona", true, 15,3,  15, "ad"), 0);
+        produkty.put(new Produkt("jabłka",false,10,2, 15,"ad"),0);
+        produkty.put(new Produkt ("gruszki", false, 12,2,  15, "ad"), 0);
+        produkty.put(new Produkt("winogrona", false, 15,3,  15, "ad"), 0);
         //odzwierzęce
-        produkty.put(new Produkt("mleko", true, 15, 3, 10, "ad"), 0);
-        produkty.put(new Produkt("jajka", true, 16, 4, 10, "ad"), 0);
-        produkty.put(new Produkt("mięso", true, 25, 6, 10,"ad"), 0);
+        produkty.put(new Produkt("mleko", false, 15, 3, 10, "ad"), 0);
+        produkty.put(new Produkt("jajka", false, 16, 4, 10, "ad"), 0);
+        produkty.put(new Produkt("mięso", false, 25, 6, 10,"ad"), 0);
         //przetwory
-        produkty.put(new Produkt("chleb pszenny", true, 44, 11, 10, "ad"), 0);
-        produkty.put(new Produkt("chleb żytni", true, 45, 11, 10, "ad"), 0);
-        produkty.put(new Produkt("dżem jabłkowy", true, 37, 9, 8, "ad"), 0);
-        produkty.put(new Produkt("dżem gruszkowy", true, 38, 9, 8, "ad"), 0);
-        produkty.put(new Produkt("sok jabłkowy", true, 35, 8, 9, "ad"), 0);
-        produkty.put(new Produkt("sok gruszkowy", true, 36, 9, 9, "ad"), 0);
-        produkty.put(new Produkt("masło", true, 40, 10, 10, "ad"), 0);
-        produkty.put(new Produkt("szynka", true, 55, 13, 12, "ad"), 0);
-        produkty.put(new Produkt("kiełbasa", true, 56, 14, 12, "ad"), 0);
+        produkty.put(new Produkt("chleb pszenny", false, 44, 11, 10, "ad"), 0);
+        produkty.put(new Produkt("chleb żytni", false, 45, 11, 10, "ad"), 0);
+        produkty.put(new Produkt("dżem jabłkowy", false, 37, 9, 8, "ad"), 0);
+        produkty.put(new Produkt("dżem gruszkowy", false, 38, 9, 8, "ad"), 0);
+        produkty.put(new Produkt("sok jabłkowy", false, 35, 8, 9, "ad"), 0);
+        produkty.put(new Produkt("sok gruszkowy", false, 36, 9, 9, "ad"), 0);
+        produkty.put(new Produkt("masło", false, 40, 10, 10, "ad"), 0);
+        produkty.put(new Produkt("szynka", false, 55, 13, 12, "ad"), 0);
+        produkty.put(new Produkt("kiełbasa", false, 56, 14, 12, "ad"), 0);
 
     }
 
@@ -101,6 +101,26 @@ public class Stodola {
                 return produkt.getCena();
         }
         return 0;
+    }
+    
+    public int getCenaWytworzeniaProduktu(String nazwa)
+    {
+        for (Produkt produkt : produkty.keySet())
+        {
+            if (produkt.getNazwa().equals(nazwa))
+                return produkt.getCenaWytworzenia();
+        }
+        return 0;
+    }
+    
+    public Produkt getProdukt(String nazwa)
+    {
+        for (Produkt produkt : produkty.keySet())
+        {
+            if (produkt.getNazwa().equals(nazwa))
+                return produkt;
+        }
+        return null;
     }
 
 }
