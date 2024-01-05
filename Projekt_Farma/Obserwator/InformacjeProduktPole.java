cde368763b7c61cf9641846f8414a5725c0760f2
Sunariz <113854410+Sunariz@users.noreply.github.com>
2024-01-05 17:18:00 +0100

    Zmiany aby sadzenie i zbieranie działało

--- Projekt_Farma/Obserwator/InformacjeProduktPole.java
+++ Projekt_Farma/Obserwator/InformacjeProduktPole.java
@@ -5,23 +5,76 @@ import java.io.File;
 public class InformacjeProduktPole implements Obserwator{
 	
 	private boolean czyProdukuje = false; // jezeli nic nie jest produkowane na polu to mozna wyswietlac pole bez ikony produktu
-	private String nazwa; // nazwa produktu produkowanego na polu
+	private String nazwa = ""; // nazwa produktu produkowanego na polu
 	private boolean GotoweDoZebrania; // czy zebranie produktu jest mozliwe
-	private int PozostalyCzas;
+	private long PozostalyCzas;
 	private File ikonka; // ikonka produktu
 	
-	@Override
-	public void aktualizacja(String nazwa, boolean GotoweDoZebrania, int PozostalyCzas, File ikonka) {
+	public InformacjeProduktPole() {
 		this.nazwa = nazwa;
 		this.GotoweDoZebrania = GotoweDoZebrania;
 		this.PozostalyCzas = PozostalyCzas;
 		this.ikonka = ikonka;
-		if (nazwa != null) {
-			czyProdukuje = true;
-		}
-		else {
-			czyProdukuje = false;
+		if (!nazwa.equals("")) {
+		    czyProdukuje = true;
+		} else {
+		    czyProdukuje = false;
 		}
 	}
+	
+	@Override
+	public void aktualizacja(String nazwa, boolean GotoweDoZebrania, long PozostalyCzas, File ikonka) {
+		this.nazwa = nazwa;
+		this.GotoweDoZebrania = GotoweDoZebrania;
+		this.PozostalyCzas = PozostalyCzas;
+		this.ikonka = ikonka;
+		if (!nazwa.equals("")) {
+		    czyProdukuje = true;
+		} else {
+		    czyProdukuje = false;
+		}
+	}
+
+	public boolean isCzyProdukuje() {
+		return czyProdukuje;
+	}
+
+	public void setCzyProdukuje(boolean czyProdukuje) {
+		this.czyProdukuje = czyProdukuje;
+	}
+
+	public String getNazwa() {
+		return nazwa;
+	}
+
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+
+	public boolean isGotoweDoZebrania() {
+		return GotoweDoZebrania;
+	}
+
+	public void setGotoweDoZebrania(boolean gotoweDoZebrania) {
+		GotoweDoZebrania = gotoweDoZebrania;
+	}
+
+	public long getPozostalyCzas() {
+		return PozostalyCzas;
+	}
+
+	public void setPozostalyCzas(int pozostalyCzas) {
+		PozostalyCzas = pozostalyCzas;
+	}
+
+	public File getIkonka() {
+		return ikonka;
+	}
+
+	public void setIkonka(File ikonka) {
+		this.ikonka = ikonka;
+	}
+	
+	
 
 }
