71f0f0b9c05ecdec8f587e7eb7fada24a83765a9
Sunariz <113854410+Sunariz@users.noreply.github.com>
2024-01-07 00:41:34 +0100

    Update Obserwator.java

--- Projekt_Farma/Obserwator/Obserwator.java
+++ Projekt_Farma/Obserwator/Obserwator.java
@@ -2,6 +2,8 @@ package Obserwator;
 
 import java.io.File;
 
+import zwierzeta.Zwierze;
+
 public interface Obserwator {
 
     public void aktualizacja(String s, boolean b, long i, File f, Zwierze zwierze, boolean czyZagroda);
