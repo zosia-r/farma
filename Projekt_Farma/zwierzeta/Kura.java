49391d4bf695359b44823f1a7e5750fa37bc7231
Sunariz <113854410+Sunariz@users.noreply.github.com>
2024-01-07 00:32:26 +0100

    Update Kura.java

--- Projekt_Farma/zwierzeta/Kura.java
+++ Projekt_Farma/zwierzeta/Kura.java
@@ -3,15 +3,19 @@ package zwierzeta;
 import Produkt.Produkt;
 
 public class Kura extends Zwierze {
-
+	
+	
 
     public Kura() {
         super();
+        this.setAdresIkonkigotowe("grafika/kuryGotowe.png");
+        this.setAdresIkonkiJedzace("grafika/kuryJedzace.png");
+        this.setAdresIkonkiGlodne("grafika/kury.png");
     }
 
     public void znies() {
         if (getGlodne() == false) {
-            setProduktZ(new ProduktZw("jajka", false, 16, 4, 10, "ad"));
+            setProduktZ(new ProduktZw("jajka", false, 16, 4, 10, "grafika/jajko.png"));
             setGlodne(true);
         }
 
