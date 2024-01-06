dad855c1fcf3699396ecd0e0129949f149b6b8e8
Sunariz <113854410+Sunariz@users.noreply.github.com>
2024-01-07 00:31:37 +0100

    Update Krowa.java

--- Projekt_Farma/zwierzeta/Krowa.java
+++ Projekt_Farma/zwierzeta/Krowa.java
@@ -3,17 +3,20 @@ package zwierzeta;
 import Produkt.Produkt;
 
 public class Krowa extends Zwierze {
-
+	
     public Krowa()
     {
         super();
+        this.setAdresIkonkigotowe("grafika/krowyGotowe.png");
+        this.setAdresIkonkiJedzace("grafika/krowyJedzace.png");
+        this.setAdresIkonkiGlodne("grafika/krowy.png");
     }
 
     public void dojenie()
     {
         if (getGlodne() == false)
         {
-            setProduktZ(new ProduktZw("mleko", false, 15, 3, 10, "ad"));
+            setProduktZ(new ProduktZw("mleko", false, 15, 3, 10, "grafika/mleko.png"));
             setGlodne(true);
         }
 
