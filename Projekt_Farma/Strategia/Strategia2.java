ce49e3286877b1057cfd35e0fdb01a2c4471612f
zosia-r <148854197+zosia-r@users.noreply.github.com>
2024-01-13 14:09:53 +0100

    niszczenie upraw

--- Projekt_Farma/Strategia/Strategia2.java
+++ Projekt_Farma/Strategia/Strategia2.java
@@ -1,8 +1,43 @@
 package Strategia;
 
-public class Strategia2 implements Strategia {
+import GUI.OkienkoKatastrofa;
+import Gra.Gra;
+import Kawalek_Ziemi.Kawalek_Ziemi;
+import Kawalek_Ziemi.Pole_Uprawne;
+
+public class Strategia2 implements Strategia
+{
+    private final Kawalek_Ziemi [][] kawalkiZiemi;
+    private final String komunikat;
+    public Strategia2(String komunikat)
+    {
+        kawalkiZiemi = Gra.getInstance().getFarmaGracza().getKawalki_ziemi();
+        this.komunikat = komunikat;
+        katastrofa();
+    }
+
+
     public void katastrofa()
     {
-        //Implementcja zniszczenia upraw
+        boolean okienko=false;
+
+        for(int i = 0; i<kawalkiZiemi.length; i++)
+        {
+            for(int j= 0; j<kawalkiZiemi[i].length; j++)
+            {
+                if(kawalkiZiemi[i][j] instanceof Pole_Uprawne)
+                {
+                    if(((Pole_Uprawne) kawalkiZiemi[i][j]).get_uprawa() != null)
+                    {
+                        ((Pole_Uprawne) kawalkiZiemi[i][j]).przrwijWzrostUprawy();
+                        okienko=true;
+                    }
+                }
+            }
+        }
+        if(okienko)
+        {
+            OkienkoKatastrofa okienkoKatastrofa = new OkienkoKatastrofa(komunikat);
+        }
     }
 }
