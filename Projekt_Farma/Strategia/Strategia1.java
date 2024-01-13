3bae08dfe8d2c6c6002103e9e0993ed7ba95ca05
KrzysiuSpacerowicz <146785513+KrzysiuSpacerowicz@users.noreply.github.com>
2024-01-13 18:49:24 +0100

    Update Strategia1.java

--- Projekt_Farma/Strategia/Strategia1.java
+++ Projekt_Farma/Strategia/Strategia1.java
@@ -1,8 +1,21 @@
 package Strategia;
 
+import GUI.OkienkoKatastrofa;
+import Gra.Gra;
+import Stodola.Stodola;
+
 public class Strategia1 implements Strategia {
-    public void katastrofa()
-    {
-        //Implementcja spalenia stodoly
+    private final String komunikat;
+
+    public Strategia1(String komunikat) {
+        this.komunikat = komunikat;
+        if (Gra.getInstance().getPozostalyCzas() > 1) {
+            katastrofa();
+        }
+    }
+    public void katastrofa() {
+        Stodola stodola = Gra.getInstance().getFarmaGracza().getStodola();
+        stodola.usunWszystkieProdukty();
+        OkienkoKatastrofa okienkoKatastrofa = new OkienkoKatastrofa(komunikat);
     }
 }
